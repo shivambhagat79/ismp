6118b6cc0e4053310898cd751c9c1a64a1b3a4c5
shivambhagat79 <shivambhagat2279+github@gmail.com>
2023-07-27 19:16:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import LocomotiveScroll from 'locomotive-scroll';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ismp';
+
+  scroll: LocomotiveScroll;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+    });
+  }
 }
